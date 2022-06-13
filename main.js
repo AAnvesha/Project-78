ca3f1c7a10ea14692b6225cb3d597c533eb09d80
Anvesha Misra <98341927+AAnvesha@users.noreply.github.com>
2022-06-13 18:22:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
 var i = 0;
 function update()
 {
@@ -11,21 +11,12 @@ function update()
       }
     
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [i];
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/MKdhy06Z/family.jpg").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/qqyYvVbq/grandpa.jpg").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/wjMnFtMX/father.jpg").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/5ymDKL83/bro.jpg").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/JnL6wtrd/sister.jpg").src = updatedImage;
-    document.getElementById("https://i.postimg.cc/bw5W5zSK/mother.jpg").src = updatedImage;
+   updatedImage=images[i];
+  
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [i] ;
+   updatedName =names[i] ;
+    
+    document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
-    document.getElementById("family book").innerHTML = updatedName;
-    document.getElementById("Ranbir Singh").innerHTML = updatedName;
-    document.getElementById("Diljeet Singh").innerHTML = updatedName;
-    document.getElementById("Rocky Singh").innerHTML = updatedName;
-    document.getElementById("Alia Singh").innerHTML = updatedName;
-    document.getElementById("Soni Singh").innerHTML = updatedName;
+
 }
